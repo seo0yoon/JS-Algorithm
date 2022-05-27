475d60d9868f4634635cf34e15f1cb989be71900
seo0yoon <genious921225@gmail.com>
2022-05-27 14:25:24 +0900

    문제 해결

--- 6번 문제/6번문제.js
+++ 6번 문제/6번문제.js
@@ -1,16 +1,14 @@
 function solution(arr) {
-  let answer = []; //최종결과값을 넣어주는 아이
-  let odd = []; //홀수를 구하는거
-  let sum = 0; //홀수 더한거
+  let answer = [];
+  let sum = 0;
+  let odd = [];
+  let min = Number.MAX_SAFE_INTEGER;
+
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] % 2 !== 0) {
-      //배열의 i번째를 2로 나눴을때 나머지가 0이 아닌거 === 홀수
-      odd.push(arr[i]); //배열의 i번째가 홀수면 odd 배열에 push 해줘
+      sum = sum + arr[i];
+      odd.push(arr[i]);
     }
-  }
-  let min = odd[0]; //홀수 배열 중에 최소값 구하기
-  for (let i = 0; i < odd.length; i++) {
-    sum = sum + odd[i];
   }
   for (let i = 0; i < odd.length; i++) {
     if (min > odd[i]) {
